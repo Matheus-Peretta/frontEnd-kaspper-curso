0359cca59b502f5f3bf92e41ffad91ff1ab4f2d7
MatheusPeretta <matheusperetta.saopaulo@gmail.com>
2023-04-04 20:22:57 -0300

    começando poo

---
+++ JavaScript/07-poo/js/01.js
@@ -0,0 +1,15 @@
+let carro = {
+    marca: 'Renault',
+    modelo: 'Sandero',
+    ano: 2022
+}
+console.log(carro.marca)
+
+let pessoa = {
+    nome: '',
+    idade: 0,
+    peso: 90,
+    casado: true
+}
+console.log('Nome: ' + pessoa.nome);
+console.log('Idade: ' + pessoa.idade);
---
+++ JavaScript/07-poo/js/02.js
@@ -0,0 +1,9 @@
+let conta = {
+    titular: 'Matheus',
+    CPF: 2542658525,
+    numConta: 12345,
+    saldo: 330
+}
+
+console.log(conta.titular)
+console.log(conta.CPF)
